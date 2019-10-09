51612b74544f105cb647a19fba0e0a49dbd293ec
Turnerj <git@turnerj.com>
2019-10-09 22:59:04 +1030

    Added initial tests for CacheStack

---
+++ tests/CacheTower.Tests/CacheStackTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CacheTower.Tests
+{
+	[TestClass]
+	public class CacheStackTests
+	{
+		[TestMethod]
+		public async Task Cleanup_CleansAllTheLayers()
+		{
+			var layer1 = new MemoryCacheLayer();
+			var layer2 = new MemoryCacheLayer();
+
+			var cacheStack = new CacheStack(null, new[] { layer1, layer2 });
+			var cacheEntry = await cacheStack.Set("Cleanup_CleansAllTheLayers", 42, TimeSpan.FromDays(-1));
+
+			Assert.AreEqual(cacheEntry, await layer1.Get<int>("Cleanup_CleansAllTheLayers"));
+			Assert.AreEqual(cacheEntry, await layer2.Get<int>("Cleanup_CleansAllTheLayers"));
+
+			await cacheStack.Cleanup(TimeSpan.Zero);
+
+			Assert.IsNull(await layer1.Get<int>("Cleanup_CleansAllTheLayers"));
+			Assert.IsNull(await layer2.Get<int>("Cleanup_CleansAllTheLayers"));
+		}
+
+		[TestMethod]
+		public async Task Evict_EvictsAllTheLayers()
+		{
+			var layer1 = new MemoryCacheLayer();
+			var layer2 = new MemoryCacheLayer();
+
+			var cacheStack = new CacheStack(null, new[] { layer1, layer2 });
+			var cacheEntry = await cacheStack.Set("Evict_EvictsAllTheLayers", 42, TimeSpan.FromDays(1));
+
+			Assert.AreEqual(cacheEntry, await layer1.Get<int>("Evict_EvictsAllTheLayers"));
+			Assert.AreEqual(cacheEntry, await layer2.Get<int>("Evict_EvictsAllTheLayers"));
+
+			await cacheStack.Evict("Evict_EvictsAllTheLayers");
+
+			Assert.IsNull(await layer1.Get<int>("Evict_EvictsAllTheLayers"));
+			Assert.IsNull(await layer2.Get<int>("Evict_EvictsAllTheLayers"));
+		}
+
+		[TestMethod]
+		public async Task Set_SetsAllTheLayers()
+		{
+			var layer1 = new MemoryCacheLayer();
+			var layer2 = new MemoryCacheLayer();
+
+			var cacheStack = new CacheStack(null, new[] { layer1, layer2 });
+			var cacheEntry = await cacheStack.Set("Set_SetsAllTheLayers", 42, TimeSpan.FromDays(1));
+
+			Assert.AreEqual(cacheEntry, await layer1.Get<int>("Set_SetsAllTheLayers"));
+			Assert.AreEqual(cacheEntry, await layer2.Get<int>("Set_SetsAllTheLayers"));
+		}
+
+
+		[TestMethod]
+		public async Task Get_BackPropagatesToEarlierCacheLayers()
+		{
+			var layer1 = new MemoryCacheLayer();
+			var layer2 = new MemoryCacheLayer();
+			var layer3 = new MemoryCacheLayer();
+
+			var cacheStack = new CacheStack(null, new[] { layer1, layer2, layer3 });
+
+			var cacheEntry = new CacheEntry<int>(42, TimeSpan.FromDays(1));
+			await layer2.Set("Get_BackPropagatesToEarlierCacheLayers", cacheEntry);
+
+			var cacheEntryFromStack = await cacheStack.Get<int>("Get_BackPropagatesToEarlierCacheLayers");
+			Assert.AreEqual(cacheEntry, cacheEntryFromStack);
+			Assert.AreEqual(cacheEntry, await layer1.Get<int>("Get_BackPropagatesToEarlierCacheLayers"));
+			Assert.IsNull(await layer3.Get<int>("Get_BackPropagatesToEarlierCacheLayers"));
+		}
+	}
+}
