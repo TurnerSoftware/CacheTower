d61aefba561c8912f9b8dd562d9d35405c80685c
Turnerj <git@turnerj.com>
2019-10-10 23:31:33 +1030

    Add benchmark for MemoryCacheLayer

---
+++ tests/CacheTower.Benchmarks/CacheLayers/MemoryCacheBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace CacheTower.Benchmarks.CacheLayers
+{
+	[CoreJob, MemoryDiagnoser]
+	public class MemoryCacheComparisonBenchmark : BaseCacheLayerBenchmark
+	{
+		[Params(1000, 100000)]
+		public int Iterations;
+
+		[Benchmark]
+		public async Task MemoryCacheGet()
+		{
+			await GetCacheBenchmark(new MemoryCacheLayer(), Iterations);
+		}
+		[Benchmark]
+		public async Task MemoryCacheSet()
+		{
+			await SetCacheBenchmark(new MemoryCacheLayer(), Iterations);
+		}
+	}
+}
