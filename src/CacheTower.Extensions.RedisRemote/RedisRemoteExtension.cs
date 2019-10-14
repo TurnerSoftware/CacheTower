ee414344cc4c7c13b21e8ada43911cfc883d02a7
Turnerj <git@turnerj.com>
2019-10-14 17:39:52 +1030

    Added RedisRemoteExtension for remote cache eviction

---
+++ src/CacheTower.Extensions.RedisRemote/RedisRemoteExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace CacheTower.Extensions.RedisRemote
+{
+	public class RedisRemoteExtension : IValueRefreshExtension
+	{
+		private ConnectionMultiplexer ConnectionMultiplexer { get; }
+		private ISubscriber Subscriber { get; }
+		private string ValueRefreshChannel { get; }
+
+		public RedisRemoteExtension(ConnectionMultiplexer connectionMultiplexer, string channelPrefix = "CacheTower")
+		{
+			ConnectionMultiplexer = connectionMultiplexer;
+			Subscriber = ConnectionMultiplexer.GetSubscriber();
+			ValueRefreshChannel = $"{channelPrefix}.ValueRefresh";
+		}
+
+		public async Task OnValueRefreshAsync(string cacheKey, TimeSpan timeToLive)
+		{
+			await Subscriber.PublishAsync(ValueRefreshChannel, cacheKey, CommandFlags.FireAndForget);
+		}
+
+		public void Register(ICacheStack cacheStack)
+		{
+			Subscriber.Subscribe(ValueRefreshChannel, async (channel, value) =>
+			{
+				string cacheKey = value;
+				await cacheStack.EvictAsync(cacheKey);
+			}, CommandFlags.FireAndForget);
+		}
+	}
+}
