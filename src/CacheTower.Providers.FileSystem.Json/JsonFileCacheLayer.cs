53742644e9893619acb015f19184ea922271c053
Turnerj <git@turnerj.com>
2019-10-27 16:35:39 +1030

    Improving JSON perf/allocations via semi-custom serialization
    Performs custom serialization on the outer wrapper but otherwise, falls back to the built-in stuff.

--- src/CacheTower.Providers.FileSystem.Json/JsonFileCacheLayer.cs
+++ src/CacheTower.Providers.FileSystem.Json/JsonFileCacheLayer.cs
@@ -5,13 +5,13 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Nito.AsyncEx;
 
 namespace CacheTower.Providers.FileSystem.Json
 {
 	public class JsonFileCacheLayer : FileCacheLayerBase<ManifestEntry>, ICacheLayer
 	{
+		private static readonly JsonSerializer Serializer = new JsonSerializer();
+
 		public JsonFileCacheLayer(string directoryPath) : base(directoryPath, ".json") { }
 
 		private class DataWrapper<T>
@@ -24,15 +24,25 @@ namespace CacheTower.Providers.FileSystem.Json
 			using (var streamReader = new StreamReader(stream, Encoding.UTF8, false, 1024))
 			using (var jsonReader = new JsonTextReader(streamReader))
 			{
-				var serializer = new JsonSerializer();
-				var wrapper = serializer.Deserialize<DataWrapper<T>>(jsonReader);
-
-				if (wrapper == default)
+				//Read start object
+				if (!jsonReader.Read() || jsonReader.TokenType != JsonToken.StartObject)
 				{
 					return default;
 				}
 
-				return wrapper.Value;
+				//Read property name
+				if (!jsonReader.Read() || jsonReader.TokenType != JsonToken.PropertyName)
+				{
+					return default;
+				}
+				
+				//Read value start
+				if (!jsonReader.Read() || jsonReader.TokenType == JsonToken.Null)
+				{
+					return default;
+				}
+
+				return Serializer.Deserialize<T>(jsonReader);
 			}
 		}
 
@@ -41,14 +51,24 @@ namespace CacheTower.Providers.FileSystem.Json
 			using (var streamWriter = new StreamWriter(stream, Encoding.UTF8, 1024, true))
 			using (var jsonWriter = new JsonTextWriter(streamWriter))
 			{
-				var wrapper = new DataWrapper<T>
-				{
-					Value = value
-				};
+				jsonWriter.WriteStartObject();
+				jsonWriter.WritePropertyName("Value");
 
-				var serializer = new JsonSerializer();
-				serializer.Serialize(jsonWriter, wrapper);
-				jsonWriter.Flush();
+				if (value is null)
+				{
+					jsonWriter.WriteNull();
+				}
+				else if (value.GetType().IsValueType || value is string)
+				{
+					jsonWriter.WriteValue(value);
+				}
+				else
+				{
+					Serializer.Serialize(jsonWriter, value);
+				}
+
+				jsonWriter.WriteEndObject();
+				
 				jsonWriter.CloseOutput = false;
 			}
 		}
