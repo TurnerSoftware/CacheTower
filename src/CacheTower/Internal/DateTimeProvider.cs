1907c7f74f6ea3f019bef824c1ae3161a28ea281
Turnerj <git@turnerj.com>
2021-03-03 12:07:46 +1030

    Updated DateTimeProvider for external updating
    Also introduced documentation to explain the logic a bit better.

--- src/CacheTower/Internal/DateTimeProvider.cs
+++ src/CacheTower/Internal/DateTimeProvider.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace CacheTower.Internal
 {
 	internal static class DateTimeProvider
 	{
-		public static DateTime Now { get; internal set; }
+		/// <summary>
+		/// The current <see cref="DateTime.UtcNow"/>, updated every second.
+		/// </summary>
+		public static DateTime Now { get; private set; }
+
+		/// <summary>
+		/// Updates <see cref="Now"/> to the current <see cref="DateTime.UtcNow"/> value. This is automatically called by a timer every second.
+		/// </summary>
+		/// <remarks>
+		/// This is intended to only be triggered by the internal timer or by unit tests that require it.
+		/// The reason why tests need it is due to the fast turn around of setting a value and testing the outcome.
+		/// Real applications aren't immediately setting a cache value manually, calling <see cref="ICacheStack.GetOrSetAsync{T}(string, Func{T, System.Threading.Tasks.Task{T}}, CacheSettings)"/> and then comparing whether the results are the same.
+		/// The alternative for the tests is just "waiting" an extra second between setting a value and retrieving it however that makes the testing slower and the tests more confusing.
+		/// </remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void UpdateTime() => Now = DateTime.UtcNow;
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "Establishes timer and prevents it being garbage collected")]
-		private static readonly Timer DateTimeTimer = new(state => Now = DateTime.UtcNow, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+		private static readonly Timer DateTimeTimer = new(state => UpdateTime(), null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
 	}
 }
