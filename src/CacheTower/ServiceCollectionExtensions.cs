ad7b85789e1910793f9d95fab20485f832518436
Turnerj <git@turnerj.com>
2021-04-22 16:10:37 +0930

    New AddCacheStack method with updated inline docs

--- src/CacheTower/ServiceCollectionExtensions.cs
+++ src/CacheTower/ServiceCollectionExtensions.cs
@@ -12,9 +12,9 @@ namespace Microsoft.Extensions.DependencyInjection
 		/// <summary>
 		/// Adds a <see cref="CacheStack"/> singleton to the specified <see cref="IServiceCollection"/> with the given layers and automatic cleanup frequency (via <see cref="AutoCleanupExtension"/>).
 		/// </summary>
-		/// <param name="services"></param>
-		/// <param name="layers"></param>
-		/// <param name="cleanupFrequency"></param>
+		/// <param name="services">The services collection to add the dependencies to.</param>
+		/// <param name="layers">The cache layers to use.</param>
+		/// <param name="cleanupFrequency">The frequency at which cache stack cleanup will be performed.</param>
 		public static void AddCacheStack(this IServiceCollection services, ICacheLayer[] layers, TimeSpan cleanupFrequency)
 		{
 			services.AddSingleton<ICacheStack>(new CacheStack(layers, new[] { new AutoCleanupExtension(cleanupFrequency) }));
@@ -22,11 +22,22 @@ namespace Microsoft.Extensions.DependencyInjection
 
 		/// <summary>
 		/// Adds a <see cref="CacheStack"/> singleton to the specified <see cref="IServiceCollection"/> with the specified layers and extensions.
+		/// </summary>
+		/// <param name="services">The services collection to add the dependencies to.</param>
+		/// <param name="layers">The cache layers to use.</param>
+		/// <param name="extensions">The cache extensions to use.</param>
+		public static void AddCacheStack(this IServiceCollection services, ICacheLayer[] layers, ICacheExtension[] extensions)
+		{
+			services.AddSingleton<ICacheStack>(new CacheStack(layers, extensions));
+		}
+
+		/// <summary>
+		/// Adds a <see cref="CacheStack"/> singleton to the specified <see cref="IServiceCollection"/> with the specified layers and extensions.
 		/// An implementation factory of <typeparamref name="TContext"/> is built using the <see cref="IServiceProvider"/> established when instantiating the <see cref="CacheStack{TContext}"/>.
 		/// </summary>
-		/// <param name="services"></param>
-		/// <param name="layers"></param>
-		/// <param name="extensions"></param>
+		/// <param name="services">The services collection to add the dependencies to.</param>
+		/// <param name="layers">The cache layers to use.</param>
+		/// <param name="extensions">The cache extensions to use.</param>
 		public static void AddCacheStack<TContext>(this IServiceCollection services, ICacheLayer[] layers, ICacheExtension[] extensions)
 		{
 			services.AddSingleton<ICacheStack<TContext>, CacheStack<TContext>>(sp => {
@@ -38,10 +49,10 @@ namespace Microsoft.Extensions.DependencyInjection
 		/// <summary>
 		/// Adds a <see cref="CacheStack{TContext}"/> singleton to the specified <see cref="IServiceCollection"/> with the specified <paramref name="contextFactory"/>, layers and extensions.
 		/// </summary>
-		/// <param name="services"></param>
-		/// <param name="contextFactory"></param>
-		/// <param name="layers"></param>
-		/// <param name="extensions"></param>
+		/// <param name="services">The services collection to add the dependencies to.</param>
+		/// <param name="contextFactory">The factory method that will generate a context when <see cref="CacheStack{TContext}.GetOrSetAsync{T}(string, Func{T, TContext, System.Threading.Tasks.Task{T}}, CacheSettings)"/> is called.</param>
+		/// <param name="layers">The cache layers to use.</param>
+		/// <param name="extensions">The cache extensions to use.</param>
 		public static void AddCacheStack<TContext>(this IServiceCollection services, Func<TContext> contextFactory, ICacheLayer[] layers, ICacheExtension[] extensions)
 		{
 			services.AddSingleton<ICacheStack<TContext>, CacheStack<TContext>>(sp => {
