3d961e3ab2b2003a40095066969dd82da50e9366
Mike Goodfellow <mike@audiu.net>
2021-08-19 09:16:20 +0100

    Add RedisBusyLockExtension

---
+++ src/CacheTower.Extensions.Redis/RedisBusyLockOptions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace CacheTower.Extensions.Redis
+{
+	/// <summary>
+	/// Lock options for use by the <see cref="RedisBusyLockExtension"/>.
+	/// </summary>
+	public class RedisBusyLockOptions
+	{
+		/// <summary>
+		/// The default options for <see cref="RedisBusyLockExtension"/>.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// - <see cref="LockTimeout"/>: 1 minute<br/>
+		/// - <see cref="SpinTime"/>: 100 milliseconds<br/>
+		/// - <see cref="DatabaseIndex"/>: <i>The default database configured on the connection.</i>
+		/// </para>
+		/// </remarks>
+		public static readonly RedisBusyLockOptions Default = new RedisBusyLockOptions();
+
+		/// <summary>
+		/// The database index used for the Redis lock.
+		/// If not specified, uses the default database as configured on the connection.
+		/// </summary>
+		public int DatabaseIndex { get; }
+
+		/// <summary>
+		/// How long to wait on the lock before having it expire.
+		/// </summary>
+		public TimeSpan LockTimeout { get; }
+
+		/// <summary>
+		/// How open to recheck the lock when we were not the context that acquired the lock
+		/// </summary>
+		public TimeSpan SpinTime { get; }
+
+		/// <summary>
+		/// Number of attempts to check the lock before giving up
+		/// </summary>
+		public int SpinAttempts { get; }
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="RedisBusyLockOptions"/>.
+		/// </summary>
+		/// <param name="lockTimeout">How long to wait on the lock before having it expire. Defaults to 1 minute.</param>
+		/// <param name="spinTime">How long to wait between rechecking the lock. Defaults to 100 milliseconds.</param>
+		/// <param name="databaseIndex">
+		/// The database index used for the Redis lock.
+		/// If not specified, uses the default database as configured on the connection.
+		/// </param>
+		public RedisBusyLockOptions(
+			TimeSpan? lockTimeout = default,
+			TimeSpan? spinTime = default,
+			int databaseIndex = -1
+		)
+		{
+			LockTimeout = lockTimeout ?? TimeSpan.FromMinutes(1);
+			SpinTime = spinTime ?? TimeSpan.FromMilliseconds(100);
+			SpinAttempts = (int)Math.Ceiling(LockTimeout.TotalMilliseconds / SpinTime.TotalMilliseconds);
+			DatabaseIndex = databaseIndex;
+		}
+	}
+}
---
+++ tests/CacheTower.Tests/Extensions/Redis/RedisBusyLockExtensionTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Threading.Tasks;
+using CacheTower.Extensions.Redis;
+using CacheTower.Tests.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CacheTower.Tests.Extensions.Redis
+{
+	[TestClass]
+	public class RedisBusyLockExtensionTests
+	{
+		[TestMethod, ExpectedException(typeof(ArgumentNullException))]
+		public void ThrowForNullConnection()
+		{
+			new RedisBusyLockExtension(null);
+		}
+
+		[TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void ThrowForInvalidDatabaseIndex()
+		{
+			new RedisBusyLockExtension(RedisHelper.GetConnection(), new RedisBusyLockOptions(databaseIndex: -10));
+		}
+
+		[TestMethod, ExpectedException(typeof(InvalidOperationException))]
+		public void ThrowForRegisteringTwoCacheStacks()
+		{
+			var extension = new RedisBusyLockExtension(RedisHelper.GetConnection());
+			var cacheStack = new Mock<ICacheStack>().Object;
+			extension.Register(cacheStack);
+			extension.Register(cacheStack);
+		}
+
+		[TestMethod]
+		public async Task RefreshValueClearsLock()
+		{
+			RedisHelper.ResetState();
+
+			var connection = RedisHelper.GetConnection();
+
+			var cacheStackMock = new Mock<ICacheStack>();
+			var extension = new RedisBusyLockExtension(connection, RedisBusyLockOptions.Default);
+			extension.Register(cacheStackMock.Object);
+
+			var cacheEntry = new CacheEntry<int>(13, TimeSpan.FromDays(1));
+
+			await extension.WithRefreshAsync("TestKey",
+				() => new ValueTask<CacheEntry<int>>(cacheEntry), new CacheSettings(TimeSpan.FromDays(1)));
+
+			var lockStatus = await connection.GetDatabase().LockQueryAsync("TestKey_lock");
+
+			Assert.IsFalse(lockStatus.HasValue, "Lock was not cleared after refreshValue");
+		}
+
+
+		[TestMethod]
+		public async Task ObservedLockSingle()
+		{
+			RedisHelper.ResetState();
+
+			var connection = RedisHelper.GetConnection();
+
+			var cacheStackMock = new Mock<ICacheStack>();
+			var extension = new RedisBusyLockExtension(connection, RedisBusyLockOptions.Default);
+			extension.Register(cacheStackMock.Object);
+
+			var cacheEntry = new CacheEntry<int>(13, TimeSpan.FromDays(1));
+
+			//Establish lock
+			await connection.GetDatabase().LockTakeAsync("TestKey_lock", "token", TimeSpan.FromMinutes(1));
+
+			var refreshTask = extension.WithRefreshAsync("TestKey",
+					() =>
+					{
+						return new ValueTask<CacheEntry<int>>(cacheEntry);
+					},
+					new CacheSettings(TimeSpan.FromDays(1))
+				).AsTask();
+
+			//Delay to allow for Redis check and self-entry into lock
+			await Task.Delay(TimeSpan.FromSeconds(1));
+
+			Assert.IsTrue(extension.LockedOnKeyRefresh.ContainsKey("TestKey"), "Lock was not established");
+
+			//Trigger the end of the lock
+			await connection.GetDatabase().LockReleaseAsync("TestKey_lock", "token");
+
+			var succeedingTask = await Task.WhenAny(refreshTask, Task.Delay(TimeSpan.FromSeconds(10)));
+			if (!succeedingTask.Equals(refreshTask))
+			{
+				RedisHelper.DebugInfo(connection);
+				Assert.Fail("Refresh has timed out - something has gone very wrong");
+			}
+
+			cacheStackMock.Verify(c => c.GetAsync<int>("TestKey"), Times.Exactly(2), "Two checks to the cache stack are expected");
+		}
+
+
+		[TestMethod]
+		public async Task ObservedLockMultiple()
+		{
+			RedisHelper.ResetState();
+
+			var connection = RedisHelper.GetConnection();
+
+			var cacheStackMock = new Mock<ICacheStack>();
+			var extension = new RedisBusyLockExtension(connection, RedisBusyLockOptions.Default);
+			extension.Register(cacheStackMock.Object);
+
+			var cacheEntry = new CacheEntry<int>(13, TimeSpan.FromDays(1));
+
+			//Establish lock
+			await connection.GetDatabase().LockTakeAsync("TestKey_lock", "token", TimeSpan.FromMinutes(1));
+
+			var refreshTask1 = extension.WithRefreshAsync("TestKey",
+					() =>
+					{
+						return new ValueTask<CacheEntry<int>>(cacheEntry);
+					},
+					new CacheSettings(TimeSpan.FromDays(1))
+				).AsTask();
+
+			var refreshTask2 = extension.WithRefreshAsync("TestKey",
+					() =>
+					{
+						return new ValueTask<CacheEntry<int>>(cacheEntry);
+					},
+					new CacheSettings(TimeSpan.FromDays(1))
+				).AsTask();
+
+			//Delay to allow for Redis check and self-entry into lock
+			await Task.Delay(TimeSpan.FromSeconds(2));
+
+			Assert.IsTrue(extension.LockedOnKeyRefresh.ContainsKey("TestKey"), "Lock was not established");
+
+			//Trigger the end of the lock
+			await connection.GetDatabase().LockReleaseAsync("TestKey_lock", "token");
+
+			var whenAllRefreshesTask = Task.WhenAll(refreshTask1, refreshTask2);
+			var succeedingTask = await Task.WhenAny(whenAllRefreshesTask, Task.Delay(TimeSpan.FromSeconds(10)));
+			if (!succeedingTask.Equals(whenAllRefreshesTask))
+			{
+				RedisHelper.DebugInfo(connection);
+				Assert.Fail("Refresh has timed out - something has gone very wrong");
+			}
+
+			cacheStackMock.Verify(c => c.GetAsync<int>("TestKey"), Times.Exactly(4), "Two checks to the cache stack are expected");
+		}
+	}
+}
